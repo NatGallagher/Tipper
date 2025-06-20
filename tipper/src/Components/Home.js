f8c0062f0993576192e063f6149b5034843aea1a
NatGallagher <natgallagher@Nats-MacBook-Air.local>
2025-06-20 18:16:32 -0400

    edited Home.js with functions and onclick

--- tipper/src/Components/Home.js
+++ tipper/src/Components/Home.js
@@ -1,5 +1,27 @@
+import { useEffect, useState } from "react"
 
 function Home() {
+
+    const [total, setTotal] = useState("");
+
+    function smallTip() {
+        let msg = "smallTip";
+        console.log(msg);
+        setTotal("18%")
+    }
+
+    function mediumTip() {
+        let msg = "mediumTip";
+        console.log(msg); 
+        setTotal("20%")
+    }
+
+    function largeTip() {
+        let msg = "largeTip";
+        console.log(msg);
+        setTotal("22%")
+    }
+
   return (
     <div>
       <header>
@@ -7,9 +29,12 @@ function Home() {
       </header>
       <div>
         <input></input><br/>
-        <button>18%</button>
-        <button>20%</button>
-        <button>22%</button>
+        <button onClick={smallTip}>18%</button>
+        <button onClick={mediumTip}>20%</button>
+        <button onClick={largeTip}>22%</button>
+      </div>
+      <div>
+        {total}
       </div>
     </div>
   );
