ea7b17759e63823495179946ae293fdb9207ff93
NatGallagher <natgallagher@Nats-MacBook-Air.local>
2025-06-20 18:37:56 -0400

    edited home.js with functions showing percentage

--- tipper/src/Components/Home.js
+++ tipper/src/Components/Home.js
@@ -1,25 +1,32 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState, useRef } from "react"
 
 function Home() {
 
     const [total, setTotal] = useState("");
+    const input = useRef(null);
 
     function smallTip() {
         let msg = "smallTip";
         console.log(msg);
-        setTotal("18%")
+        const inputValue = input.current.value;
+        const tipped = (inputValue * 18) / 100
+        setTotal(tipped)
     }
 
     function mediumTip() {
         let msg = "mediumTip";
         console.log(msg); 
-        setTotal("20%")
+        const inputValue = input.current.value;
+        const tipped = (inputValue * 20) / 100
+        setTotal(tipped)
     }
 
     function largeTip() {
         let msg = "largeTip";
         console.log(msg);
-        setTotal("22%")
+        const inputValue = input.current.value;
+        const tipped = (inputValue * 22) / 100
+        setTotal(tipped)
     }
 
   return (
@@ -28,7 +35,7 @@ function Home() {
         <h1>Tipper</h1>
       </header>
       <div>
-        <input></input><br/>
+        <input ref={input}></input><br/>
         <button onClick={smallTip}>18%</button>
         <button onClick={mediumTip}>20%</button>
         <button onClick={largeTip}>22%</button>
