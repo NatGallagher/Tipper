0c580a038b55d721e0e267c6949473413643ec34
NatGallagher <natgallagher@Nats-Air.attlocal.net>
2025-06-26 16:10:44 -0400

    made it through npm build

---
+++ tipper/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.natg.tipper',
+  appName: 'Tipper',
+  webDir: 'build'
+};
+
+export default config;
